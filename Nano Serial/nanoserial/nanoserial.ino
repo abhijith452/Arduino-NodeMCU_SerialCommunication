1be3f1f09d18b51a397372cd517f7e761e27903e
Abhijith Kannan <abhijithkannan452@gmail.com>
2020-03-03 22:12:06 +0530

    First Commit

---
+++ Nano Serial/nanoserial/nanoserial.ino
@@ -0,0 +1,54 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial s(5,6);
+char value_1[10] ="111" ;
+char value_2[10]  ="2.6" ;
+char value_3[10]  ="3.6" ;
+void setup() 
+{
+
+  Serial.begin(9600);
+s.begin(9600);
+
+
+}
+
+void loop()
+{
+
+
+
+
+
+
+send_to_nodemcu ();
+  
+}
+
+
+void send_to_nodemcu ()
+{
+  int a = s.read();
+  if(a==5)
+  {
+s.write(value_1,10);
+  }
+  else if(a==6)
+  {
+s.write(value_2,10);
+
+  }
+  else if(a==7)
+  {
+s.write(value_3,10);
+
+  Serial.println("MESSAGE SENT");
+  }
+  else
+  {
+  
+  Serial.println("No msg");
+  }
+  
+}
+
---
+++ NoduMCU/nODEmCU/nODEmCU.ino
@@ -0,0 +1,56 @@
+String  value_1;
+String  value_2;
+String  value_3 ;
+
+
+
+#include <SoftwareSerial.h>
+SoftwareSerial s(D6,D5);
+
+ 
+void setup() {
+ 
+ 
+  Serial.begin(9600);
+  s.begin(9600);
+ 
+ 
+}
+ 
+void loop() {
+ 
+
+ 
+
+ 
+
+
+  from_arduino ();
+
+
+   Serial.println(value_1);
+ Serial.println(value_2);
+  Serial.println(value_3);
+}
+
+
+
+void from_arduino ()
+{
+
+
+s.write(5);
+
+    value_1 = (s.readString());
+   
+
+s.write(6);
+    value_2 = (s.readString());
+     
+
+s.write(7);
+    value_3 = (s.readString());
+     
+ 
+
+}
